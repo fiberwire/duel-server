10fa18742f94e42f4880d612419e7f924a221569
fiberwire <andrew.ramos92@gmail.com>
2016-04-06 20:27:55 -0400

    test server code

--- server.go
+++ server.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+    "html"
+	"net/http"
+	"log"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        
+    })
+    
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
